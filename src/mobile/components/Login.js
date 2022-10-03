a8fa74883588ea7d2d9aa2ea8cd40ccd6a354f56
Naman Arora <namanarora166@gmail.com>
2022-10-03 17:01:06 -0500

    Update Login.js

--- src/mobile/components/Login.js
+++ src/mobile/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Input, Text, View, VStack } from 'native-base';
 import { StyleSheet, useColorScheme } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Login = () => {
   const colorScheme = useColorScheme();
@@ -39,61 +40,64 @@ const Login = () => {
   });
   return (
     <>
-      <VStack space={3} style={styles.vStackStyles}>
-        <Text fontSize="4xl" style={styles.titleStyles}>
-          CareersGO
-        </Text>
-        <Text fontSize="xl" style={styles.subtitleStyles}>
-          Lets create a CareersGO account!
-        </Text>
-        <View>
-          <Text style={styles.defaultTextStyles}>Your First Name:</Text>
-          <Input
-            variant="rounded"
-            placeholder=" Naman"
-            size="lg"
-            style={styles.inputStyles}
-            value={firstName}
-            onChange={e => setFirstName(e.currentTarget.value)}
-          />
-        </View>
-        <View>
-          <Text style={styles.defaultTextStyles}>Your Last Name:</Text>
-          <Input
-            variant="rounded"
-            placeholder=" Arora"
-            size="lg"
-            style={styles.inputStyles}
-            value={lastName}
-            onChange={e => setLastName(e.currentTarget.value)}
-          />
-        </View>
-        <View>
-          <Text style={styles.defaultTextStyles}>Your Email:</Text>
-          <Input
-            variant="rounded"
-            placeholder=" namanarora@example.com"
-            size="lg"
-            style={styles.inputStyles}
-            value={email}
-            onChange={e => setEmail(e.currentTarget.value)}
-          />
-        </View>
-        <View>
-          <Text style={styles.defaultTextStyles}>Create a Password:</Text>
-          <Input
-            variant="rounded"
-            placeholder=" ··················"
-            size="lg"
-            style={styles.inputStyles}
-            value={password}
-            onChange={e => setPassword(e.currentTarget.value)}
-          />
-        </View>
-        <View>
-          <Button style={styles.buttonStyles}>Create an Account!</Button>
-        </View>
-      </VStack>
+      <SafeAreaView>
+        <VStack space={3} style={styles.vStackStyles}>
+          <Text fontSize="4xl" style={styles.titleStyles}>
+            CareersGO
+          </Text>
+          <Text fontSize="xl" style={styles.subtitleStyles}>
+            Lets create a CareersGO account!
+          </Text>
+          <View>
+            <Text style={styles.defaultTextStyles}>Your First Name:</Text>
+            <Input
+              variant="rounded"
+              placeholder=" Naman"
+              size="lg"
+              style={styles.inputStyles}
+              value={firstName}
+              onChange={e => setFirstName(e.currentTarget.value)}
+            />
+          </View>
+          <View>
+            <Text style={styles.defaultTextStyles}>Your Last Name:</Text>
+            <Input
+              variant="rounded"
+              placeholder=" Arora"
+              size="lg"
+              style={styles.inputStyles}
+              value={lastName}
+              onChange={e => setLastName(e.currentTarget.value)}
+            />
+          </View>
+          <View>
+            <Text style={styles.defaultTextStyles}>Your Email:</Text>
+            <Input
+              variant="rounded"
+              placeholder=" namanarora@example.com"
+              size="lg"
+              style={styles.inputStyles}
+              value={email}
+              onChange={e => setEmail(e.currentTarget.value)}
+            />
+          </View>
+          <View>
+            <Text style={styles.defaultTextStyles}>Create a Password:</Text>
+            <Input
+              variant="rounded"
+              placeholder=" ··················"
+              size="lg"
+              style={styles.inputStyles}
+              value={password}
+              onChange={e => setPassword(e.currentTarget.value)}
+            />
+          </View>
+          <View>
+            <Button style={styles.buttonStyles}>Create an Account!</Button>
+          </View>
+        </VStack>
+        <Text style={styles.defaultTextStyles}>{firstName}</Text>
+      </SafeAreaView>
     </>
   );
 };
